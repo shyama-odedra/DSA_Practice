9867778374ead8efe34e68665934e61f4feb721c
shyama odedra <shyamaodedra9@gmail.com>
2026-07-23 08:40:57 +0530

    Time: 1 ms (99.72%), Space: 64.2 MB (67.93%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -0,0 +1,18 @@
+class RecentCounter {
+
+private:
+    queue<int> q;
+
+public:
+    RecentCounter() {
+    }
+    
+    int ping(int t) {
+        q.push(t);
+
+        while (!q.empty() && q.front() < t-3000) {
+            q.pop();
+        }
+        return q.size();
+    }
+};
