e6c5c3ed8a1e59b348f98d77a669a3d030357792
shyama odedra <shyamaodedra9@gmail.com>
2026-07-23 08:42:39 +0530

    Time: 15 ms (67.38%), Space: 64.1 MB (96.17%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -1,16 +1,13 @@
 class RecentCounter {
-
-private:
+public:
     queue<int> q;
 
-public:
     RecentCounter() {
     }
-    
     int ping(int t) {
         q.push(t);
 
-        while (!q.empty() && q.front() < t-3000) {
+        while (!q.empty() && q.front() < t - 3000) {
             q.pop();
         }
         return q.size();
