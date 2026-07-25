50b27cf0e758c88d03e1271f12352a868146090d
shyama odedra <shyamaodedra9@gmail.com>
2026-07-25 13:56:04 +0530

    Time: 0 ms (100%), Space: 11.5 MB (57.06%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> radiant, dire;
+        int n = senate.length();
+
+        for (int i = 0; i < n; i++) {
+            if (senate[i] == 'R') {
+                radiant.push(i);
+            } else {
+                dire.push(i);
+            }
+        }
+
+        while (!radiant.empty() && !dire.empty()) {
+            int r = radiant.front();
+            int d = dire.front();
+            radiant.pop();
+            dire.pop();
+
+            if (r < d) {
+                radiant.push(r + n);
+            } else {
+                dire.push(d + n);
+            }
+        }
+
+        return radiant.empty() ? "Dire" : "Radiant";
+    }
+};
