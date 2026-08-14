ad5dc23bd4cea7ab8f76cc870a4b6136c84107f2
shyama odedra <shyamaodedra9@gmail.com>
2026-08-14 11:22:46 +0530

    Time: 7 ms (98.9%), Space: 24 MB (94.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
         for (const string& s : strs) {
-            string key = s;
+            string key=s;
             sort(key.begin(), key.end());
             mp[key].push_back(s);
         }
