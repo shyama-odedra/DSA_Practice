5967a0e1997fba5c4323129016a1b799cc2a5644
shyama odedra <shyamaodedra9@gmail.com>
2026-08-24 10:19:13 +0530

    Time: 1 ms (18.79%), Space: 8.9 MB (41.17%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        stringstream ss(s);
+        string word;
+        
+        while (ss >> word) {
+            words.push_back(word);
+        }
+        
+        if (pattern.length() != words.size()) {
+            return false;
+        }
+        
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+        
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern[i];
+            string w = words[i];
+            
+            if (charToWord.count(c) && charToWord[c] != w) {
+                return false;
+            }
+            if (wordToChar.count(w) && wordToChar[w] != c) {
+                return false;
+            }
+            
+            charToWord[c] = w;
+            wordToChar[w] = c;
+        }
+        
+        return true;
+    }
+};
